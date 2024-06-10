b931807b437fa63421b3a4eaa9570d221f1f27dc
ga5brie <junsumy523@gmail.com>
2024-06-09 19:37:52 -0500

    wep

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("hola mundo")
+import express from "express"
+import {dirname, join} from "path"
+import { fileURLToPath } from "url"
+
+const app= express()
+
+const __dirname= dirname(fileURLToPath(import.meta.url))
+app.set("views", join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+
+app.get("/",(req, res)=> res.send("index"))
+app.listen(3000)
+console.log("Server Listening on port", 3000)
